ab61ac0c42bbbb5bad2ea27d9dddcad5d60fc16b
Charles <charles@Deth-Ubuntu-Box.(none)>
2014-01-27 01:59:07 -0500

    Tip calculator using sys.argv

--- tip_calculator/tip_calculator.py
+++ tip_calculator/tip_calculator.py
@@ -1,15 +1,21 @@
 import sys
 
+'''
 meal = float(raw_input("What is the base cost of meal?")) 
 tax = float(raw_input("What is the meal tax rate? (In hundredths)"))
 tip = float(raw_input("How much tip do you want to give? (In hundredths)"))
+'''
 
-tax_value = tax * meal
-meal_with_tax = meal + tax_value
-tip_value = tip * meal_with_tax
+print sys.argv[1]
+print sys.argv[2]
+print sys.argv[3]
+
+tax_value = float(sys.argv[2]) * float(sys.argv[1])
+meal_with_tax = float(sys.argv[1]) + tax_value
+tip_value = float(sys.argv[3]) * meal_with_tax
 total = meal_with_tax + tip_value
 
-print "The base cost of your meal is %r" % meal
+print "The base cost of your meal is %r" % float(sys.argv[1])
 print "The tax applied toward your meal is %r" % tax_value
-print "The tip applied toward your meal is %r, given a tip of %r percent" % (tip_value,tip)
+print "The tip applied toward your meal is %r, given a tip of %r percent" % (tip_value,float(sys.argv[3]))
 print "The total cost of your meal is %r" % total
